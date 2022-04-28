de24938c5dfd7f2c4fe33fdd7ba7ca0ffd6cbc31
Sebastien-Gineste <47150778+Sebastien-Gineste@users.noreply.github.com>
2022-04-28 16:18:46 +0200

    update game

--- main.js
+++ main.js
@@ -2,3 +2,8 @@
 // Fichier généré automatiquement par polyconquest :
 // Veuillez utiliser ce fichier pour envoyer votre code
 // -------------------------------
+
+console.log("suite 0")
+console.log("suite 1")
+console.log("suite 1")
+console.log("suite 2")
